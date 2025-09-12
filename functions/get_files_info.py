955eb475aa5ece428e96b2108f90f11b5cc0fc83
geenath <dlgpforwork24@gmail.com>
2025-09-12 12:08:48 +1200

    added restricted directory access

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,21 +4,19 @@ import os
 def get_files_info(working_directory,directory="."):
     try:
         path = os.path.join(working_directory,directory)
-        os.listdir(path)
-        itemList = os.scandir(path) 
-        print(f"Result for current directory")
+        abs_path = os.path.abspath(path)
+        if not path.startswith("calculator") or "calculator" not in abs_path:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(path):
+            return f'Error: "{directory}" is not a directory'
+        itemList = os.scandir(path)
+        out_put_string = ""
+        print(f"Result for '{directory}' directory:")
         for entry in itemList:
-            print(f" - {entry.name}: file_size={os.path.getsize(entry.path)} bytes, is_dir={entry.is_dir()}")
-    except FileNotFoundError:
+           out_put_string +=f"\t- {entry.name}: file_size={os.path.getsize(entry.path)} bytes, is_dir={entry.is_dir()}"
+           out_put_string +="\n"
+        return out_put_string
+    except FileNotFoundError as e:
+        print(e)
                 
     
-
-
-
-def main():
-    get_files_info("/home/geenath/projects/python-ai-agent/python-ai-agent-proj/functions",".")
-
-
-
-if __name__ == "__main__":
-    main()
